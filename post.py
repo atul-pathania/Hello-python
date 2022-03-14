d516b4766035a44b3d3055a2466340889ec4c395
atul-pathania <101039972+atul-pathania@users.noreply.github.com>
2022-03-14 15:06:28 +0530

    Add files via upload
    check this.. y this is not working with the upper case letters?

---
+++ post.py
@@ -0,0 +1,11 @@
+# write a program to find out whether a given post is talking about "Atul" or not
+# whether atul in upper case or lower case
+post =str(input("Enter your post: "))
+a = "atul"
+# if a.lower() in post:
+#     print("Yes")
+if a.upper() in post:
+    print("yes")
+else:
+    print("No")
+
